7611a7f454759a193f00788d8f546e273fe624e2
TheKB123 <ncris2709@gmail.com>
2026-07-29 13:47:22 +0300

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -53,7 +53,7 @@ async def user_videos():
             updated=None
             proxy = os.environ.get("PROXY_URL", None)
             async with TikTokApi() as api:
-                await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False, browser='webkit', proxy=proxy)
+                await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False, browser='webkit')
                 ttuser = api.user(user)
                 try:
                     user_data = await ttuser.info()
