aecdc80aa59684ca49fd2c74ba5ac8fc87d180a1
TheKB123 <ncris2709@gmail.com>
2026-07-29 13:55:14 +0300

    Refactor postprocessing.py for improved functionality

--- postprocessing.py
+++ postprocessing.py
@@ -53,7 +53,7 @@ async def user_videos():
             updated=None
             
             async with TikTokApi() as api:
-                await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=False, browser='webkit')
+                await api.create_sessions(ms_tokens=[ms_token], num_sessions=1, sleep_after=3, headless=True, browser='webkit')
                 ttuser = api.user(user)
                 try:
                     user_data = await ttuser.info()
